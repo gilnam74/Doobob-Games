9f206655638a245a081c1b5cd4e5947c7baa4476
gilnam74 <gilnam74@gmail.com>
2026-02-13 14:01:44 -0800

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    site: 'https://doobob.net',
+  output: 'static',
+  site: 'https://doobob.net',
 });
