2f106daa2b3aa1264325b92396c60376663d8a2e
gilnam74 <48565237+gilnam74@users.noreply.github.com>
2026-02-13 13:55:30 -0800

    Fix formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    site: 'https://doobob.net',
+  output: 'static',
+  site: 'https://doobob.net',
 });
