15a42239c3a5e3f230b7fdb1379fb6ec53d395d8
gilnam74 <48565237+gilnam74@users.noreply.github.com>
2026-02-13 13:53:25 -0800

    Clean up astro.config.mjs by removing unused imports
    Removed unused imports for netlify, react, and tailwindcss.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'static',
     site: 'https://doobob.net',
 });
